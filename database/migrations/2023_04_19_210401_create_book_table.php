a70b2e3962c2995f7c97d9035a4c76262c080e0b
Nayan Pitroda <141473201+nayan4472@users.noreply.github.com>
2024-12-22 20:35:52 +0530

    Add files via upload

---
+++ database/migrations/2023_04_19_210401_create_book_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('room_name');
+            $table->string('room_price');
+            $table->string('check-in');
+            $table->string('check-out');
+            $table->string('adult');
+            $table->string('children');
+            $table->string('additional_requests');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book');
+    }
+};
